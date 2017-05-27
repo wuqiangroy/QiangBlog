f4cbea31bcee897b9ae358bae6252a9831b48f58
wuqiangroy <wuqiangroy@live.com>
2017-05-27 15:36:32 +0800

    add config

--- config.py
+++ config.py
@@ -1,10 +1,43 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
+import os
 
-def develop():
-    POSTGRESQL_URL = ""
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-def production():
-    POSTGRESQL_URL = ""
+class Config():
+    """基础配置"""
+
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "coding change world"
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SUBJECT_PREFIX = ["QiangBlog"]
+    MAIL_SENDER = "wuqiangroy@live.com"
+    MAIL_SERVER = "smtp.live.com"
+    MAIL_PORT = 587
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = "wuqiangroy@live.com"
+    MAIL_PASSWORD = os.environ.get("PASSWORD")
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class Development(Config):
+    """开发配置"""
+
+    DEBUG = True
+    POSTGRESQL_URL = "postgresql://dbuser:password@localhost/dev_qiangblog"
+
+
+class Production(Config):
+    """线上配置"""
+
+    POSTGRESQL_URL = "postgresql://dbuser:password@localhost/pro_qiangblog"
+
+Config = {
+    "development": Development,
+    "production": Production,
+    "default": Development
+}
